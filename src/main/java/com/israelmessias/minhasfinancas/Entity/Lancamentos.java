85fd7c43e3cb0c003e3ace114b820a70ab2a8cbc
israelmessias <69429419+israelmessias@users.noreply.github.com>
2021-08-07 16:27:44 -0300

    Entity Lançamentos

--- src/main/java/com/israelmessias/minhasfinancas/Entity/Lancamentos.java
+++ src/main/java/com/israelmessias/minhasfinancas/Entity/Lancamentos.java
@@ -1,5 +1,21 @@
 package com.israelmessias.minhasfinancas.Entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="lacamentos", schema = "financas")
 public class Lancamentos {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="mes")
+    private Integer mes;
+
+    @Column(name="ano")
+    private Integer ano;
+
+    private Usuario usuario;
+
+
 }
