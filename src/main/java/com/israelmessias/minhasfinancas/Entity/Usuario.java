b42e4ede55d67e384d5361244b3f4f13e9a9b835
israelmessias <69429419+israelmessias@users.noreply.github.com>
2021-08-07 16:29:28 -0300

    Entity Usuario

---
+++ src/main/java/com/israelmessias/minhasfinancas/Entity/Usuario.java
@@ -0,0 +1,35 @@
+package com.israelmessias.minhasfinancas.Entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "usuario", schema = "financas")
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Usuario {
+
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "senha")
+    private String senha;
+
+
+}
