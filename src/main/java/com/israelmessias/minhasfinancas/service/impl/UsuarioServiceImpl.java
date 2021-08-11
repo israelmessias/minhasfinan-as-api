647b2074a05c17bce3f741bc6be5a50bc5dbfa5e
israelmessias <69429419+israelmessias@users.noreply.github.com>
2021-08-10 21:19:23 -0300

    Usuario Service

--- src/main/java/com/israelmessias/minhasfinancas/service/impl/UsuarioServiceImpl.java
+++ src/main/java/com/israelmessias/minhasfinancas/service/impl/UsuarioServiceImpl.java
@@ -1,2 +1,37 @@
-package com.israelmessias.minhasfinancas.service.impl;public class UsuarioServiceImpl {
+package com.israelmessias.minhasfinancas.service.impl;
+
+import com.israelmessias.minhasfinancas.exception.RegraNegocioException;
+import com.israelmessias.minhasfinancas.model.Entity.Usuario;
+import com.israelmessias.minhasfinancas.model.repository.UsuarioRepository;
+import com.israelmessias.minhasfinancas.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioServiceImpl implements UsuarioService {
+
+    private UsuarioRepository usuarioRepository;
+    @Autowired
+    public UsuarioServiceImpl(UsuarioRepository usuarioRepository) {
+        super();
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public Usuario autenticar(String email, String senha) {
+        return null;
+    }
+
+    @Override
+    public Usuario salvarUsuario(Usuario usuario) {
+        return null;
+    }
+
+    @Override
+    public void validarEmail(String email) {
+        boolean existe = usuarioRepository.existsByEmail(email);
+        if(existe){
+            throw new RegraNegocioException("Ja existe um usuario cadastrado!");
+        }
+    }
 }
