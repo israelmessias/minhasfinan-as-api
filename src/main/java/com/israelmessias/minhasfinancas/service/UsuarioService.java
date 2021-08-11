ab5cbb96c559e5dc78fe9722c8bdcc8d40e950a4
israelmessias <69429419+israelmessias@users.noreply.github.com>
2021-08-10 21:18:40 -0300

    Metodos para implementar no service

--- src/main/java/com/israelmessias/minhasfinancas/service/UsuarioService.java
+++ src/main/java/com/israelmessias/minhasfinancas/service/UsuarioService.java
@@ -1,2 +1,16 @@
-package com.israelmessias.minhasfinancas.service;public interface UsuarioService {
+package com.israelmessias.minhasfinancas.service;
+
+import com.israelmessias.minhasfinancas.model.Entity.Usuario;
+
+public interface UsuarioService {
+    //Metodo para autenticar a entrada do usuario no sistema
+    Usuario autenticar(String email, String senha);
+
+    //Salvar usuario
+    Usuario salvarUsuario(Usuario usuario);
+
+    //Para que o email só cadastre uma vez só
+    void validarEmail(String email);
 }
+
+
