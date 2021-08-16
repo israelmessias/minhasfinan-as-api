720be1445d31542f1705febb8b8d98c77d75f3e7
israelmessias <69429419+israelmessias@users.noreply.github.com>
2021-08-16 20:15:36 -0300

    Ainda não deu certo

--- src/test/java/com/israelmessias/minhasfinancas/model/repository/UsuarioRepositoryTest.java
+++ src/test/java/com/israelmessias/minhasfinancas/model/repository/UsuarioRepositoryTest.java
@@ -1,2 +1,30 @@
-package com.israelmessias.minhasfinancas.model.repository;public class UsuarioRepositoryTest {
+package com.israelmessias.minhasfinancas.model.repository;
+
+import com.israelmessias.minhasfinancas.model.Entity.Usuario;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+public class UsuarioRepositoryTest {
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    @Test
+    public void deveVerificarAExistenciaDeUmEmail(){
+        //Cenario
+        Usuario usuario1 = Usuario.builder().email("israel@email.com").build();
+        usuarioRepository.save(usuario1);
+        //Execução
+        boolean result = usuarioRepository.existsByEmail("israel@email.com");
+        //verificação
+    }
 }
