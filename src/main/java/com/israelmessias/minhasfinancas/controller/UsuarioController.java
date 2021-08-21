5a9b1e89fd48c4b2410ba4fe256693e75a701e6f
israelmessias <69429419+israelmessias@users.noreply.github.com>
2021-08-21 19:04:45 -0300

    Salvar usuario

--- src/main/java/com/israelmessias/minhasfinancas/controller/UsuarioController.java
+++ src/main/java/com/israelmessias/minhasfinancas/controller/UsuarioController.java
@@ -1,13 +1,50 @@
 package com.israelmessias.minhasfinancas.controller;
 
+import javax.persistence.EntityManager;
+
+import com.israelmessias.minhasfinancas.api.dto.UsuarioDTO;
+import com.israelmessias.minhasfinancas.exception.RegraNegocioException;
+import com.israelmessias.minhasfinancas.model.Entity.Usuario;
+import com.israelmessias.minhasfinancas.service.UsuarioService;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class UsuarioController {
+    private UsuarioService service;
 
-    @GetMapping("/")
-    public String HelloWorld(){
+    public UsuarioController(UsuarioService service)
+    {
+        this.service = service;
+    }
+
+    @GetMapping("/hello")
+    public String HelloWorld()
+    {
         return "Salve pivete!!!";
     }
+
+    @PostMapping("/usuarios")
+    public ResponseEntity salvar(@RequestBody UsuarioDTO usuarioDTO)
+    {
+       Usuario usuario = Usuario.builder().nome(usuarioDTO.getNome()).
+       email(usuarioDTO.getEmail()).senha(usuarioDTO.getSenha()).build();
+
+       try 
+       {
+           Usuario usuarioSalvo = service.salvarUsuario(usuario);
+           return new ResponseEntity(usuarioSalvo, HttpStatus.CREATED);
+       } catch (RegraNegocioException e) 
+       {
+           return ResponseEntity.badRequest().body(e.getMessage());
+       }
+    }
+
 }
