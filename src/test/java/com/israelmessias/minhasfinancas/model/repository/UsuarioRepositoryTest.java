7d118998b909866518bdb8f1c01bd1560fe7c4b1
israelmessias <69429419+israelmessias@users.noreply.github.com>
2021-08-20 21:25:00 -0300

    caso não exista um email

--- src/test/java/com/israelmessias/minhasfinancas/model/repository/UsuarioRepositoryTest.java
+++ src/test/java/com/israelmessias/minhasfinancas/model/repository/UsuarioRepositoryTest.java
@@ -8,12 +8,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
 public class UsuarioRepositoryTest {
 
     @Autowired
@@ -28,5 +30,17 @@ public class UsuarioRepositoryTest {
         boolean result = usuarioRepository.existsByEmail("israel@email.com");
         //verificação
         Assertions.assertThat(result).isTrue();
+    }
+
+    @Test
+    public void deveRetornarFalsoNaoHouverQuandoUsuarioForCadastradoComEmail(){
+        //cenario
+        usuarioRepository.deleteAll();
+
+        //ação
+        boolean result = usuarioRepository.existsByEmail("usuario@email.com");
+
+        //verificação
+        Assertions.assertThat(result).isFalse();
     }
 }
