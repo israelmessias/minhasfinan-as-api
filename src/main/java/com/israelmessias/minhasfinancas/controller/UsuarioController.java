773a0c22b5072dcfc92d48a263696e119ad51d31
israelmessias <69429419+israelmessias@users.noreply.github.com>
2021-08-22 19:49:35 -0300

    UsuarioController: Injeção de dependência

--- src/main/java/com/israelmessias/minhasfinancas/controller/UsuarioController.java
+++ src/main/java/com/israelmessias/minhasfinancas/controller/UsuarioController.java
@@ -6,6 +6,7 @@ import com.israelmessias.minhasfinancas.exception.RegraNegocioException;
 import com.israelmessias.minhasfinancas.model.Entity.Usuario;
 import com.israelmessias.minhasfinancas.service.UsuarioService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class UsuarioController {
+    @Autowired
     private UsuarioService service;
 
     public UsuarioController(UsuarioService service)
