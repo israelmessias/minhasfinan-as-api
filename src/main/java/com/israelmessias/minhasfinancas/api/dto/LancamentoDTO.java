db4c16ef9281f10d0bd23ce32bce125855fb6ee6
israelmessias <69429419+israelmessias@users.noreply.github.com>
2021-09-03 20:29:37 -0300

    DTO Lancamento: retirei o bug

--- src/main/java/com/israelmessias/minhasfinancas/api/dto/LancamentoDTO.java
+++ src/main/java/com/israelmessias/minhasfinancas/api/dto/LancamentoDTO.java
@@ -9,24 +9,17 @@ import javax.validation.constraints.NotBlank;
 import java.math.BigDecimal;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LancamentoDTO {
+
     private Long id;
-
     private String descricao;
-
     private Integer mes;
-
     private Integer ano;
-
     private BigDecimal valor;
-
     private Long usuario;
-
     private String tipo;
-
     private String status;
-
 }
