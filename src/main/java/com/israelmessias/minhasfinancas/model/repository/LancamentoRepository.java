94375c0af6bf17d5fe311cdce641aec8344b60e1
israelmessias <69429419+israelmessias@users.noreply.github.com>
2021-09-04 00:32:35 -0300

    Obter saldo

--- src/main/java/com/israelmessias/minhasfinancas/model/repository/LancamentoRepository.java
+++ src/main/java/com/israelmessias/minhasfinancas/model/repository/LancamentoRepository.java
@@ -1,7 +1,18 @@
 package com.israelmessias.minhasfinancas.model.repository;
 
 import com.israelmessias.minhasfinancas.model.Entity.Lancamento;
+import com.israelmessias.minhasfinancas.model.enums.TipoLancamento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.math.BigDecimal;
 
 public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
+
+    /*
+    * Vai somar todos lançamentos agrupados pelo tipo de usuario*/
+    @Query(value = "select sum(l.valor) from Lancamento l join l.usuario u " +
+            "where u.id = :idUsuario and l.tipo =:tipo group by u")
+    BigDecimal obterSaldoPorTipoLancamentoEUsuario(@Param("idUsuario") Long idUsuario, @Param("tipo") TipoLancamento tipo);
 }
