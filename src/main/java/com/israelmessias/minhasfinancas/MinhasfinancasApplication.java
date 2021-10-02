49e14b31cbca347a606080a6b3385bdd42a05bd7
israelmessias <69429419+israelmessias@users.noreply.github.com>
2021-10-02 10:56:35 -0300

    Update MinhasfinancasApplication.java

--- src/main/java/com/israelmessias/minhasfinancas/MinhasfinancasApplication.java
+++ src/main/java/com/israelmessias/minhasfinancas/MinhasfinancasApplication.java
@@ -2,7 +2,6 @@ package com.israelmessias.minhasfinancas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -11,13 +10,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 @Configuration
 @EnableWebMvc
-public class MinhasfinancasApplication implements WebMvcConfigurer {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
-            }
+public class MinhasfinancasApplication implements WebMvcConfigurer
+{
+    @Override
+    public void addCorsMappings(CorsRegistry registry) 
+    {
+        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SpringApplication.run(MinhasfinancasApplication.class, args);
     }
 
